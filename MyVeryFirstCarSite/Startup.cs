f8a26ba6a905a002ac67f6c0ad54e793a80f5041
Cian Skelly <cianskelly@hotmail.com>
2018-10-18 20:52:49 +0100

    Add project files.

---
+++ MyVeryFirstCarSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyVeryFirstCarSite.Startup))]
+namespace MyVeryFirstCarSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
