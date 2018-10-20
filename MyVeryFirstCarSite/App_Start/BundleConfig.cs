6cc1635b1d8c5a51fad4a8c8c7ca797ac67a7924
Cian Skelly <cianskelly@hotmail.com>
2018-10-20 13:36:04 +0100

    added the admin to the navbar and the hover element to it, and changed the layout.cshtml files in the shared and admin area render section.../bundles/admin

--- MyVeryFirstCarSite/App_Start/BundleConfig.cs
+++ MyVeryFirstCarSite/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace MyVeryFirstCarSite
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                      "~/Scripts/AdminMenu.js"));
         }
     }
 }
