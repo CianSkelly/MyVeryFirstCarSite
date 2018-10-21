3206496451f0e37f4a73df983a64599b1f35e52c
Cian Skelly <cianskelly@hotmail.com>
2018-10-21 17:25:50 +0100

    I modified the AspNetUsers table by adding properties to the ApplicationUser class in the IdentityModels class. These new properties are refelcted as columns in the AspNetUsers table - FirstName, IsActive and DateWhenRegistered columns.

--- MyVeryFirstCarSite/Models/IdentityModels.cs
+++ MyVeryFirstCarSite/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -10,6 +11,11 @@ namespace MyVeryFirstCarSite.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        //the following property specifies if a user is active or not
+        public bool IsActive { get; set; }
+        public DateTime Registered { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
