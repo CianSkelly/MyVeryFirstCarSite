34548ab0f10f169b919aafaea08f8823478b49b0
Cian Skelly <cianskelly@hotmail.com>
2018-10-23 19:35:49 +0100

    I created section controller in the admin area (controllers folder) and 5 views - 1.Index, 2. Create new, 3. Edit, 4. Details & 5. Delete. These 5 were created for each of the items i the "Admin Section" dropdown. I also created a partial view for the create new button so I could re-use this button from other views. Same for the edit, details, delete and back to list buttons. The smallButton model was created so it could be re-used as a model by all buttons. Styling added to table annd buttone, etc.

---
+++ MyVeryFirstCarSite/Areas/Admin/Controllers/SectionController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyVeryFirstCarSite.Entities;
+using MyVeryFirstCarSite.Models;
+
+namespace MyVeryFirstCarSite.Areas.Admin.Controllers
+{
+    public class SectionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Section
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Sections.ToListAsync());
+        }
+
+        // GET: Admin/Section/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // GET: Admin/Section/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Section/Create - used when we fill in data in the box on the create view, 
+        //and click the create button. The data is sent back to the server from the clients' input
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //the "Bind" below mean that only ID & Title are allowed to be sent in to this action. 
+        //Prevents malicous code entering.
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                //1st - section object added to the section table
+                db.Sections.Add(section);
+                //then await the result from the SaveChangesAsync method which adds the data to the DB 
+                //once no exception thrown
+                await db.SaveChangesAsync();
+                //then redirect to the index action so the data that has been added can be viewed
+                return RedirectToAction("Index");
+            }
+            //else re-render the view with the error data so we can try re-create
+            return View(section);
+        }
+
+        // GET: Admin/Section/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Edit/5 - use this view to add data to the DB
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //again, action object sent in and only allow Id & title
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                //telling the DB that it ahs beeen edited...
+                db.Entry(section).State = EntityState.Modified;
+                //as above
+                await db.SaveChangesAsync();
+                //see the changes
+                return RedirectToAction("Index");
+            }
+            //else re-render the view
+            return View(section);
+        }
+
+        // GET: Admin/Section/Delete/5 - delete the item with only the Id sent in
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Section section = await db.Sections.FindAsync(id);
+            db.Sections.Remove(section);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyVeryFirstCarSite/Areas/Admin/Models/SmallButtonModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MyVeryFirstCarSite.Areas.Admin.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        //the int below is nullable due to the "?"
+        public int? Id { get; set; }
+        public int? ItemId { get; set; }
+        public int? VehicleId { get; set; }
+        public int? SubscriptionId { get; set; }
+
+        public string ActionParameters {
+            get {
+                //seeded with an initial value of the "?". When I build the URL, anything to the right of the "?" 
+                //will be a list of parameters, sent with the URL to the server
+                var param = new StringBuilder("?");
+                if (Id != null && Id > 0)
+                    param.Append(String.Format("{0}={1}&", "id", Id));
+
+                if (ItemId != null && Id > 0)
+                    param.Append(String.Format("{0}={1}&", "itemId", ItemId));
+
+                if (VehicleId != null && Id > 0)
+                    param.Append(String.Format("{0}={1}&", "vehicleId", VehicleId));
+
+                if (SubscriptionId != null && Id > 0)
+                    param.Append(String.Format("{0}={1}&", "subscriptionId", SubscriptionId));
+
+                return param.ToString().Substring(0, param.Length - 1);
+            }
+        }
+    }
+}
